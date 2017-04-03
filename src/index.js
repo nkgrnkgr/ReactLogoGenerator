536484d7a7b59ea409e918fdfcd913de2140e7d7
nokogiring <beefupme@gmail.com>
2017-04-04 06:12:09 +0900

    finished minhello

--- src/index.js
+++ src/index.js
@@ -3,20 +3,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// 各種モジュールのimport
-import HelloWorld from './HelloWorld/index';
-import PropsState from './PropsState/index'
-import Page from './Page/index';
-import LogoGenerator from "./LogoGenerator/container/Main/index";
-
 // HTMLのdomに唯一アクセスする箇所
 const el = document.getElementById('root')
 
 // htmlの#rootに作成されたDomを描画している
 ReactDom.render(
-    // <PropsState defaultValue="twitter" />,
-    // <HelloWorld />,
-
     // ミニマムHello HelloWorld
     <h1>Hello World</h1>,
     el
